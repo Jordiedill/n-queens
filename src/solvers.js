9e27260ea2f8c50f8b1577f0453a999233b4fdb7
jordiedill <jordan.dillard13@gmail.com>
2019-06-06 15:27:15 -0500

    Garrett/Jordan - progress made on n-rooks solver

--- src/solvers.js
+++ src/solvers.js
@@ -16,25 +16,78 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
   //have a counter variable for how many rooks have been placed 
+  let counter = 0;
+  //have an empty array to store column indexes on the board already
+  let columns = []; //[0]
+ 
+  //create a new board that is nxn
+  let ourBoard = new Board({n:n});
+  let solution;
+  
 
-  //loop through possible column index numbers
-    //try placing a rook at row 0[index] -- first rook will obviously pass
+  let looper = function() {
+    //loop over rows from 0 to n
+    for (var row = 0; row < n; row++) { //row is at 1
+      //if we're in the first row...
+      if (row === 0) {
+        //add a piece at the "starting column" in the first row
+        ourBoard.togglePiece(row,startingColumn);
+        columns.push(startingColumn);
+        counter++;
+        continue;
+      }
+      for (var col=0; col < n; col++) { //col = 1
+        if (columns.indexOf(col) > -1) {
+          continue;
+        }
+        ourBoard.togglePiece(row,col);
+        if (ourBoard.hasAnyRooksConflicts() === true) {
+          ourBoard.togglePiece(row,col);
+          continue
+        } else {
+          columns.push(col);
+          counter++
+          break;
+        }
+        
+      }
+      continue;
+    }
+    if (counter === n) {
+      solution = ourBoard.rows();
+    } else {
+      counter = 0;
+      startingColumn++;
+      columns = [];
+      return looper();
+    }
+  }
 
-    //skip to next row and place a new rook in the next column. Each time rook is placed successfully, increment counter by 1
-
-    //skip to next row...
-
-    //When we hit the last row, if counter variable equals n, return board as a solution. Otherwise, start again with new starting position
+  looper()
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  let solutionCount = 0; 
+  let permute = function(n) {
+    let numbers = Array.from(Array(n).keys())
+    let result = []
+    let recurse = function(options,n) {
+      if (n===0) {
+        result.push(options)
+      } else {
+        for(let x=0; x <numbers.length; x++) {
+          recurse(options.concat(numbers[x], n -1))
+        }
+      }
+    }
+    recurse([],n);
+    return result;
 
+  }
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
